84b5a6e35880a4b51500b69f442122275dd5e896
Test USer <testex@abc.com>
2023-03-13 22:35:30 +0500

    Add project files.

---
+++ BlazorWASApp/Model/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorWASApp.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RollNumber { get; set; }
+
+    }
+}
