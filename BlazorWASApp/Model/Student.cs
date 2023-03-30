9fe43c4c6950fc78e4eefb3ba77a3b25816a8732
Test USer <testex@abc.com>
2023-03-30 23:18:25 +0500

    -form data -valdiation

--- BlazorWASApp/Model/Student.cs
+++ BlazorWASApp/Model/Student.cs
@@ -1,10 +1,17 @@
-﻿namespace BlazorWASApp.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWASApp.Model
 {
     public class Student
     {
+        [Required]
         public int Id { get; set; }
+        [StringLength(50 , ErrorMessage ="name limit exceeds ",MinimumLength =5)]
         public string Name { get; set; }
+        
         public string RollNumber { get; set; }
+        
+        public DateTime DOB { get; set; }
 
     }
 }
